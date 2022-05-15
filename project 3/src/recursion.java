61da53858b772d8a6bccb02a90fe92279ef2a249
SUSHMITHA K <SUSHMITHA K@LAPTOP-905KQ1HS>
2022-05-15 13:26:13 +0530

    first comit

---
+++ project 3/src/recursion.java
@@ -0,0 +1,20 @@
+
+public class recursion {
+
+	public static void main(String[] args); 
+			static int count=0;  
+			static void p(){  
+			count++;  
+			if(count<=5){  
+			System.out.println("hello "+count);  
+			p();  
+			}  
+			}  
+			public static void main1(String[] args) {  
+			p();  
+			}  
+  
+
+	}
+
+
